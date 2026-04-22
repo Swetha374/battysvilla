11ed6d9680886a4684e8cc6bddbfc9602adff6fa
Swetha374 <swethasasi374@gmail.com>
2026-04-22 22:25:22 +0530

    Updates

---
+++ server/assets/_tanstack-start-manifest_v-XLVTs2fP.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/bumblebee/Documents/cudiort/battys-villa/battys-villa-escape-main/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/battysvilla/assets/index-ILxAkfpg.js"] }, "/": { filePath: "C:/Users/bumblebee/Documents/cudiort/battys-villa/battys-villa-escape-main/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/battysvilla/assets/index-BnGuPpFW.js"] } }, clientEntry: "/battysvilla/assets/index-ILxAkfpg.js" });
+export {
+  tsrStartManifest
+};
